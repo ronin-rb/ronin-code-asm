89b9e93d55d83eba5d89232fbe035292d343a016
Postmodern <postmodern.mod3@gmail.com>
2012-08-26 19:50:09 -0700

    Speced Shellcode#assemble.

--- spec/shellcode_spec.rb
+++ spec/shellcode_spec.rb
@@ -3,5 +3,26 @@ require 'ronin/asm/shellcode'
 
 describe ASM::Shellcode do
   describe "#assemble" do
+    subject do
+      described_class.new do
+        xor   eax,  eax
+        push  eax
+        push  0x68732f2f
+        push  0x6e69622f
+        mov   esp,  ebx
+        push  eax
+        push  ebx
+        mov   esp,  ecx
+        xor   edx,  edx
+        mov   0xb,  al
+        int   0x80
+      end
+    end
+
+    let(:shellcode) { "f1\xC0fPfh//shfh/binf\x89\xE3fPfSf\x89\xE1f1\xD2\xB0\v\xCD\x80" }
+
+    it "assemble down to raw machine code" do
+      subject.assemble.should == shellcode
+    end
   end
 end
