9751eadc427c41d07bfdb2d7b94a909e07c224e8
TheNotary <no@mail.plz>
2013-04-16 15:54:10 -0500

    filtering texts tagged 'integration' now instead of 'yasm'

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,5 +7,5 @@ include Ronin::ASM
 
 RSpec.configure do |specs|
   specs.treat_symbols_as_metadata_keys_with_true_values = true
-  specs.filter_run_excluding :yasm
+  specs.filter_run_excluding :integration
 end
