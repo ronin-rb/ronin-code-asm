7e144524bb6ac47838ce76cdb0d9852268a84a01
Postmodern <postmodern.mod3@gmail.com>
2021-12-23 19:15:03 -0800

    Use the repository name in the license headers.

--- lib/ronin/asm/os/linux.rb
+++ lib/ronin/asm/os/linux.rb
@@ -1,22 +1,22 @@
 #
-# Ronin ASM - A Ruby DSL for crafting Assembly programs and Shellcode.
+# ronin-asm - A Ruby DSL for crafting Assembly programs and Shellcode.
 #
 # Copyright (c) 2007-2021 Hal Brodigan (postmodern.mod3 at gmail.com)
 #
 # This file is part of ronin-asm.
 #
-# Ronin Asm is free software: you can redistribute it and/or modify
+# ronin-asm is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Ronin Asm is distributed in the hope that it will be useful,
+# ronin-asm is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Ronin Asm.  If not, see <https://www.gnu.org/licenses/>
+# along with ronin-asm.  If not, see <https://www.gnu.org/licenses/>
 #
 
 require 'ronin/asm/config'
