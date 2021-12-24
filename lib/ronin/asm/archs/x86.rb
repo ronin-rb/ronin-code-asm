ba3a997471398f48dd9ad725dc77311677a289ce
Postmodern <postmodern.mod3@gmail.com>
2021-12-23 19:13:43 -0800

    Project name in the license headers should be Ronin Asm.

--- lib/ronin/asm/archs/x86.rb
+++ lib/ronin/asm/archs/x86.rb
@@ -5,18 +5,18 @@
 #
 # This file is part of ronin-asm.
 #
-# Ronin is free software: you can redistribute it and/or modify
+# Ronin Asm is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Ronin is distributed in the hope that it will be useful,
+# Ronin Asm is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Ronin.  If not, see <https://www.gnu.org/licenses/>
+# along with Ronin Asm.  If not, see <https://www.gnu.org/licenses/>
 #
 
 require 'ronin/asm/register'
