40cc2cf8665bc8aa9331288e5946109f43b6d46b
Postmodern <postmodern.mod3@gmail.com>
2022-09-01 18:48:56 -0700

    Added a spec for `Ronin::ASM.new`.

--- spec/asm_spec.rb
+++ spec/asm_spec.rb
@@ -1,8 +1,14 @@
 require 'spec_helper'
-require 'ronin/asm/version'
+require 'ronin/asm'
 
 describe ASM do
   it "should have a version" do
     expect(subject.const_defined?('VERSION')).to eq(true)
+  end
+
+  describe ".new" do
+    it "must return a new Ronin::ASM::Program" do
+      expect(subject.new(arch: :x86)).to be_kind_of(Ronin::ASM::Program)
+    end
   end
 end
